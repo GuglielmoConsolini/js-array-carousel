1a23615e3479be7cfbd37b383cf9e58257516b47
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-23 20:50:40 +0200

    M4 evento iterazione inversa

--- README.md
+++ README.md
@@ -8,4 +8,10 @@
 
 3 mi creo delle variabili , rispettivamente l'immagine in visione e quella successiva , senza dargli un valore.
 
-4 ora mi creo un ciclo per far scorrere le immagini ad ogni refresh. Creando la variabile immagine che sarà uguale al nome dell'array più il contatore i.
+4 ora mi creo un ciclo per far scorrere le immagini ad ogni refresh. Creando la variabile immagine che sarà uguale alnome dell'array più il contatore i. 
+
+5 Dopo aver testato che il ciclo funzioni in maniera corretta(togliendo la classe active e aggiungerla all'immagine successiva)
+faccio in modo che l'iterazione del ciclo avvenga ad ogni click su una freccia attraverso .addEventListener
+
+6 per ottenere al click l'effetto contrario rifaccio un altro addeventlistener con lo stesso codice cambiando la variabile in previous e successivamente dichiaro che imagePrev è uguale all'immagine attuale -1 e con la condizione che se imagePrev sia minore di 0 le riassegno il valore dicendo che è uguale alla lunghezza dell'array -1
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let imageActive;
 let imageNext;
+let imagePrev
+
 
 let listaImmagini = document.getElementsByClassName("slide");
 console.log(listaImmagini);
@@ -32,24 +34,33 @@ document.getElementById("next").addEventListener("click" , function(){
 
 })
 
-// for (let i = 0; i < listaImmagini.length; i++){
-
-//     const immagine = listaImmagini[i];
-//     console.log(i , immagine)
-
-//     if(immagine.classList.contains("active")){
-
-//         immagine.classList.remove("active")
-//         imageActive = i
-//     }
-
-// }
-
-// imageNext = imageActive + 1;
-
-// if (imageNext >= listaImmagini.length){
-//     imageNext = 0;
-// }
 
 
-// listaImmagini[imageNext].classList.add("active");
+
+document.getElementById("prev").addEventListener("click" , function(){
+
+
+    for (let x = 0; x < listaImmagini.length; x++){
+
+        const previous = listaImmagini[x];
+        console.log(x , previous)
+    
+        if(previous.classList.contains("active")){
+    
+            previous.classList.remove("active")
+            imageActive = x
+        }
+    
+    }
+    
+    imagePrev = imageActive - 1;
+    
+    if (imagePrev < 0){
+        imagePrev = listaImmagini.length - 1;
+    }
+    
+    
+    listaImmagini[imagePrev].classList.add("active");
+
+})
+
